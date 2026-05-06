5fc09ac4dab35140ba4103d18fb9518a45a218ca
Tomas Pytel <tomas.pytel12@gmail.com>
2026-05-06 13:24:31 +0200

    Added sidebar with mock labels, refactored footer and navbar, tailwind upgrade v4

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 import { Suspense } from "react";
 import Image from "next/image";
 import Logo from "./favicon.ico";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-16 items-center">
+        {/**NAVBAR */}
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-7xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
@@ -31,22 +32,11 @@ export default function Home() {
             </div>
           </div>
         </nav>
+        {/**THE HERO SECTION */}
         <Hero />
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
+        {/**FOOTER */}
+        <Footer />
       </div>
     </main>
   );
