26fbb408c8c8a339f2996e7e49d9091d5269f93d
Tomas Pytel <tomas.pytel12@gmail.com>
2026-05-08 10:57:38 +0200

    Added styling to property detail and indicators

--- app/(protected)/properties/[id]/page.tsx
+++ app/(protected)/properties/[id]/page.tsx
@@ -1,8 +1,12 @@
-import { Suspense } from "react";
 import { mockProperties } from "../page";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import PropertyHeroSection from "@/components/properties/property-hero-section";
+import { FaMoneyBills } from "react-icons/fa6";
+import IndicatorCard from "@/components/indicator-card";
+import { IoMdTrendingUp } from "react-icons/io";
+import { FiUsers } from "react-icons/fi";
 
-async function PropertyDetails({
+export default async function PropertyDetailsPage({
   params,
 }: {
   params: Promise<{ id: string }>;
@@ -10,39 +14,58 @@ async function PropertyDetails({
   const { id } = await params;
   const property = mockProperties.find((p) => p.id === Number(id));
 
-  if (!property) {
-    return <div>Property not found</div>;
-  }
+  const indicators: {
+    icon: React.ElementType;
+    title: string;
+    value: string | number;
+    topRight?: React.ReactNode;
+  }[] = [
+    {
+      icon: FaMoneyBills,
+      title: "MESAČNÝ NÁJOM",
+      value: "550€",
+      topRight: (
+        <span className="text-xs text-green-500">+15% od minulého mesiaca</span>
+      ),
+    },
+    {
+      icon: IoMdTrendingUp,
+      title: "ROČNÝ VÝNOS",
+      value: "5.82%",
+    },
+    {
+      icon: FiUsers,
+      title: "OBSADENOSŤ",
+      value: "100%",
+      topRight: (
+        <span className="text-xs text-muted-foreground">Od 01.06.2023</span>
+      ),
+    },
+  ];
 
   return (
-    <div>
-      <h1>Property Details for ID: {id}</h1>
-      <p>Title: {property.title}</p>
-      <p>Description: {property.description}</p>
-      <p>City: {property.city}</p>
-      <p>Street: {property.street}</p>
-      <p>Postal Code: {property.postalCode}</p>
-    </div>
-  );
-}
-
-export default async function PropertyDetailsPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  return (
-    <div>
+    <div className="flex h-full flex-col">
+      {/**Header */}
       <div className="flex border-b p-5 gap-2 items-center">
         <SidebarTrigger className="md:hidden" />
         <h1 className="text-2xl font-semibold tracking-tight ">
           Detail nehnuteľnosti
         </h1>
       </div>
-      <div className="p-5">
-        <Suspense fallback={<div>Loading property details...</div>}>
-          <PropertyDetails params={params} />
-        </Suspense>
+
+      {/**Content */}
+      <div className="flex-1 p-5">
+        {/**sm:grid-cols-2 */}
+        {property && <PropertyHeroSection property={property} />}
+
+        {/**Indicators */}
+        <ul className="mt-5 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 ">
+          {indicators.map((indicator, index) => (
+            <li key={index}>
+              <IndicatorCard {...indicator} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -34,7 +34,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Separator } from "./ui/separator";
 
 // This is sample data.
 const platformNav = [
@@ -91,9 +90,9 @@ export default function AppSidebar({
         </SidebarMenu>
       </SidebarHeader>
 
-      <Separator />
       {/* Content */}
       <SidebarContent>
+        {/* <Separator /> */}
         {/* Platform group */}
         <SidebarGroup>
           <SidebarGroupLabel>Platforma</SidebarGroupLabel>
