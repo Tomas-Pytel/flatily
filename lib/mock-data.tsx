648fec3320a94fd732b1703af388ce16116e09d5
Tomas-Pytel <tomas.pytel12@gmail.com>
2026-05-13 10:56:30 +0200

    Updated db schema to relate with signed in users and fetching of properties from db in properties page (#19)

--- lib/mock-data.tsx
+++ lib/mock-data.tsx
@@ -1,78 +1,77 @@
 import { Indicator } from "@/app/(protected)/properties/[id]/page";
-import { DocumentInfo, Property } from "@/types/property";
+import { DocumentInfo } from "@/types/property";
 import { TenantInfo } from "@/types/tenant";
 import { Banknote, FileText, TrendingUp, Users } from "lucide-react";
 
-export const mockProperties: Property[] = [
-  {
-    id: 1,
-    title: "Moderný 2-izbový byt v centre mesta",
-    description: "Priestranný byt s balkónom a výhľadom na park.",
-    city: "Bratislava",
-    street: "Šancová 12",
-    postalCode: "81105",
-    img: "/hero-img.jpg",
-  },
-  {
-    id: 2,
-    title: "Útulná garsónka blízko centra",
-    description: "Ideálne pre jednotlivca, kompletne zariadená.",
-    city: "Bratislava",
-    street: "Račianska 45",
-    postalCode: "83102",
-    img: "/twitter-image.png",
-  },
-  {
-    id: 3,
-    title: "Rodinný dom so záhradou",
-    description: "Tichá lokalita, veľká záhrada a garáž.",
-    city: "Trnava",
-    street: "Jarná 8",
-    postalCode: "91701",
-  },
-  {
-    id: 4,
-    title: "Luxusný penthouse s terasou",
-    description: "Panoramatický výhľad na mesto, top lokalita.",
-    city: "Bratislava",
-    street: "Dunajská 3",
-    postalCode: "81108",
-  },
-  {
-    id: 5,
-    title: "3-izbový byt po rekonštrukcii",
-    description: "Moderná kuchyňa, blízko MHD a škôl.",
-    city: "Nitra",
-    street: "Mostná 21",
-    postalCode: "94901",
-  },
-  {
-    id: 6,
-    title: "Chata v prírode",
-    description: "Perfektné na víkendy, blízko lesa a turistiky.",
-    city: "Žilina",
-    street: "Lesná 99",
-    postalCode: "01001",
-  },
-  {
-    id: 7,
-    title: "Štýlový loft v industriálnom štýle",
-    description: "Vysoké stropy, otvorený priestor, unikátny dizajn.",
-    city: "Košice",
-    street: "Továrenská 14",
-    postalCode: "04001",
-  },
-  {
-    id: 8,
-    title: "Novostavba 4-izbového bytu",
-    description: "Energeticky úsporný, parkovanie v garáži.",
-    city: "Prešov",
-    street: "Slnečná 5",
-    postalCode: "08001",
-  },
-];
+// export const mockProperties: Property[] = [
+//   {
+//     id: 1,
+//     title: "Moderný 2-izbový byt v centre mesta",
+//     description: "Priestranný byt s balkónom a výhľadom na park.",
+//     city: "Bratislava",
+//     street: "Šancová 12",
+//     postalCode: "81105",
+//     img: "/hero-img.jpg",
+//   },
+//   {
+//     id: 2,
+//     title: "Útulná garsónka blízko centra",
+//     description: "Ideálne pre jednotlivca, kompletne zariadená.",
+//     city: "Bratislava",
+//     street: "Račianska 45",
+//     postalCode: "83102",
+//     img: "/twitter-image.png",
+//   },
+//   {
+//     id: 3,
+//     title: "Rodinný dom so záhradou",
+//     description: "Tichá lokalita, veľká záhrada a garáž.",
+//     city: "Trnava",
+//     street: "Jarná 8",
+//     postalCode: "91701",
+//   },
+//   {
+//     id: 4,
+//     title: "Luxusný penthouse s terasou",
+//     description: "Panoramatický výhľad na mesto, top lokalita.",
+//     city: "Bratislava",
+//     street: "Dunajská 3",
+//     postalCode: "81108",
+//   },
+//   {
+//     id: 5,
+//     title: "3-izbový byt po rekonštrukcii",
+//     description: "Moderná kuchyňa, blízko MHD a škôl.",
+//     city: "Nitra",
+//     street: "Mostná 21",
+//     postalCode: "94901",
+//   },
+//   {
+//     id: 6,
+//     title: "Chata v prírode",
+//     description: "Perfektné na víkendy, blízko lesa a turistiky.",
+//     city: "Žilina",
+//     street: "Lesná 99",
+//     postalCode: "01001",
+//   },
+//   {
+//     id: 7,
+//     title: "Štýlový loft v industriálnom štýle",
+//     description: "Vysoké stropy, otvorený priestor, unikátny dizajn.",
+//     city: "Košice",
+//     street: "Továrenská 14",
+//     postalCode: "04001",
+//   },
+//   {
+//     id: 8,
+//     title: "Novostavba 4-izbového bytu",
+//     description: "Energeticky úsporný, parkovanie v garáži.",
+//     city: "Prešov",
+//     street: "Slnečná 5",
+//     postalCode: "08001",
+//   },
+// ];
 
-export const tenants: TenantInfo[] = [
 export const tenants: TenantInfo[] = [
   {
     name: "Ján Novák",
